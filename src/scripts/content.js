38c69b70d5b0c60cc30e50d0efe2a714a6539a4f
Chuhj <chj9574@naver.com>
2024-03-19 17:50:39 +0900

    ♻️ Move fetch, store posts logic to service worker

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -10,43 +10,10 @@ function getUsername() {
   }
 }
 
-async function getFollowingsPosts(username) {
-  try {
-    const response = await fetch('http://localhost:3000/posts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username }),
-    });
-
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return await response.json();
-  } catch (error) {
-    console.error(`getFollowingsPosts() error: ${error.message}`);
-    throw error;
-  }
-}
-
 (async function () {
   try {
-    const lastRequestTime = sessionStorage.getItem('lastRequestTime');
-    const elapsedMinutes = (Date.now() - lastRequestTime) / (60 * 1000);
-    if (elapsedMinutes <= 10) return;
-
-    await chrome.storage.local.set({ loading: true });
-
     const username = getUsername();
     if (!username) throw new Error('Username is required.');
-
-    const posts = await getFollowingsPosts(username);
-
-    sessionStorage.setItem('lastRequestTime', Date.now());
-
-    await chrome.storage.local.set({ posts, error: false });
-    await chrome.storage.local.set({ loading: false });
   } catch (error) {
     console.error(error);
     await chrome.storage.local.set({ error: true });
